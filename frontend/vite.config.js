50f148058e8bdd59534a86bcbdadeef14a6f6fe5
riaanjlagrange <riaanjlagrange@gmail.com>
2024-09-18 22:30:08 +0200

    fix typo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server {
+  server: {
     proxy: {
       "/api": {
         target: "https://localhost:3002",
       },
-    }
-  }
+    },
+  },
 });
